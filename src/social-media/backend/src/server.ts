236fb6880555a461af3b7ac1edef04e8dfb6cfb1
BRENDA PAULINO DE ARAUJO <24010@unifacef.edu.br>
2023-05-11 20:45:41 -0300

    Adicionando front e chamando o back para pegar os posts

--- src/social-media/backend/src/server.ts
+++ src/social-media/backend/src/server.ts
@@ -1,7 +1,15 @@
 import Fastify from 'fastify'
 //import cors from '@fastify/cors'
 import { AppRoutes } from './routes'
+
+//importa dependencia cors
+import cors from '@fastify/cors'
+
+
 const server = Fastify()
+//registra cors no server
+server.register(cors)
+
 server.register(AppRoutes)
 server.listen({
     port: 3333
