822b5c8d19a2696a776d08220338d7526f66b3b2
BRENDA PAULINO DE ARAUJO <24010@unifacef.edu.br>
2023-05-17 20:41:28 -0300

    Adicionando os arquivos que inserem dados no banco

--- src/social-media/frontend/scripts.js
+++ src/social-media/frontend/scripts.js
@@ -15,3 +15,30 @@ async function consultaPosts(){
     //jogar os dados no html
     document.getElementById("corpoTabela").innerHTML = conteudoTabela
 }
+
+//consome API que cadastra um post no banco de dados
+async function confirmar(){
+    //recuperar os dados do formulario
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    const published = document.getElementById("sim").checked
+
+    const corpo = {title,content,published}
+    const post = await fetch("http://localhost:3333/post", {
+        method: 'POST',
+        body: JSON.stringify(corpo),//json transformando em string
+        headers: {//headers serve para avisar que você esta enviando um json
+            "Content-Type": "application/json;charset=UTF-8"
+        }
+
+    })
+    .then(resposta => {
+        alert("Operação realizada com sucesso")
+    })
+    .catch(error =>{
+        alert("Operação falhou")
+    })
+
+    consultaPosts()
+
+}
